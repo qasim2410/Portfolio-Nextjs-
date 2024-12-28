96c94363d4d26a96d8a77d71602c973f582fed16
Qasim <qasim.hussain2410@gmail.com>
2024-12-28 18:56:53 +0500

    add

--- src/app/component/projects.tsx
+++ src/app/component/projects.tsx
@@ -21,10 +21,11 @@ const Projects = () => {
       {/* My Projects */}
       <div className="lg:w-1/3 sm:w-1/2 p-4">
         <div className="flex relative">
-          <img
+          <Image
             alt="gallery"
             className="absolute inset-0 w-full h-full object-cover object-center"
             src='/assets/project/project-01.png'
+            layout="fill"
           />
           <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
             <h2 className="tracking-widest text-sm title-font font-medium text-blue-500 mb-1">
